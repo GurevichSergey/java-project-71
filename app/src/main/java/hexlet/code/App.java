f4b2e9672e44f72f82323614677829cb8688f26c
GurevichSergey <spakro1802@gmail.com>
2022-11-20 15:37:54 +1000

    make filepath String format

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,7 +5,6 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
-import java.io.File;
 import java.util.concurrent.Callable;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
@@ -13,10 +12,10 @@ import java.util.concurrent.Callable;
 class App implements Callable<String> {
 
     @Parameters(index = "0", description = "path to first file")
-    private File filepath1;
+    private String filepath1;
 
     @Parameters(index = "1", description = "path to second file")
-    private File filepath2;
+    private String filepath2;
 
     @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
     private String format;
