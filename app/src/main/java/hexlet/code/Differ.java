0ebcd30ee9baa0910938ab7fd5d6109698008278
GurevichSergey <spakro1802@gmail.com>
2022-11-20 15:38:24 +1000

    upd Differ

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,35 +1,30 @@
 package hexlet.code;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import java.io.File;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
 
 public class Differ {
-    public static String generate(File filepath1, File filepath2) throws Exception {
-        ObjectMapper objectMapper = new ObjectMapper();
-        var map1 = objectMapper.readValue(filepath1, new TypeReference<Map<String, Object>>() { });
-        var map2 = objectMapper.readValue(filepath2, new TypeReference<Map<String, Object>>() { });
+    public static String generate(String filepath1, String filepath2) throws Exception {
+        Map<String, Object> map1 = Parser.parse(filepath1);
+        Map<String, Object> map2 = Parser.parse(filepath2);
         var mapResult = new LinkedHashMap<String, Object>();
-        for (var key : getKey(map1, map2)) {
+        for (var key : getAllKey(map1, map2)) {
             if (!map1.containsKey(key)) {
-                mapResult.put("+" + key, map2.get(key));
+                mapResult.put("+ " + key, map2.get(key));
             } else if (!map2.containsKey(key)) {
-                mapResult.put("-" + key, map1.get(key));
+                mapResult.put("- " + key, map1.get(key));
             } else if (!map1.get(key).equals(map2.get(key))) {
-                mapResult.put("-" + key, map1.get(key));
-                mapResult.put("+" + key, map2.get(key));
+                mapResult.put("- " + key, map1.get(key));
+                mapResult.put("+ " + key, map2.get(key));
             } else {
-                mapResult.put(" " + key, map1.get(key));
+                mapResult.put("  " + key, map1.get(key));
             }
         }
         return toString(mapResult);
     }
-    private static Set<String> getKey(Map<String, Object> map1, Map<String, Object> map2) {
+    private static Set<String> getAllKey(Map<String, Object> map1, Map<String, Object> map2) {
         var allKey = new TreeSet<String>();
         allKey.addAll(map1.keySet());
         allKey.addAll(map2.keySet());
