2d8780068c476c35c5bdb1afcf16c77d7f444e5d
sergeygurevich <spakro1802@gmail.com>
2023-01-08 22:48:48 +1000

    refactoring test for Generate method

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -8,14 +8,21 @@ import java.nio.file.Paths;
 
 public class DifferTest {
 
-    private final String path1 = "src/test/resources/filepath1.json";
-    private final String path2 = "src/test/resources/filepath2.json";
-    private final Path expectedJson =
-            Paths.get("src/test/resources/expected/expectedJson").toAbsolutePath().normalize();
+    private final String pathJson1 = "src/test/resources/filepath1.json";
+    private final String pathJson2 = "src/test/resources/filepath2.json";
+    private final String pathYaml1 = "src/test/resources/filepath1.json";
+    private final String pathYaml2 = "src/test/resources/filepath2.json";
+    private final Path expectedResult =
+            Paths.get("src/test/resources/expected/expected").toAbsolutePath().normalize();
 
     @Test
-    public void testGenerate() throws Exception {
-        String expected = Files.readString(expectedJson);
-        assertEquals(expected, Differ.generate(path1, path2));
+    public void testGenerateJson() throws Exception {
+        String expected = Files.readString(expectedResult);
+        assertEquals(expected, Differ.generate(pathJson1, pathJson2));
+    }
+    @Test
+    public void testGenerateYaml() throws Exception {
+        String expected = Files.readString(expectedResult);
+        assertEquals(expected, Differ.generate(pathYaml1, pathYaml2));
     }
 }
